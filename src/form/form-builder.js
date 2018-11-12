133b53a905895431f8235f291fabe2fe7452cf10
Geoff Cox <redgeoff@gmail.com>
2018-11-11 20:13:35 -0800

    refactor(form-builder): use mson-component

--- src/form/form-builder.js
+++ src/form/form-builder.js
@@ -1,54 +1,63 @@
-export default {
-  name: 'FormBuilder',
-  component: 'Form',
-  fields: [
-    {
-      name: 'tabs',
-      component: 'Tabs',
-      items: [
-        {
-          name: 'edit',
-          label: 'Edit',
-          icon: 'Edit'
-        },
-        {
-          name: 'preview',
-          label: 'Preview',
-          icon: 'ViewCompact'
-        }
-      ]
-    },
+import MSONComponent from '../component/mson-component';
 
-    {
-      name: 'form',
-      component: 'FormField',
-      form: {
-        component: 'FormEditor'
+export default class FormBuilder extends MSONComponent {
+  _create(props) {
+    super._create(props);
+
+    this.set({
+      definition: {
+        component: 'Form',
+        fields: [
+          {
+            name: 'tabs',
+            component: 'Tabs',
+            items: [
+              {
+                name: 'edit',
+                label: 'Edit',
+                icon: 'Edit'
+              },
+              {
+                name: 'preview',
+                label: 'Preview',
+                icon: 'ViewCompact'
+              }
+            ]
+          },
+
+          {
+            name: 'form',
+            component: 'FormField',
+            form: {
+              component: 'FormEditor'
+            }
+          }
+        ],
+
+        listeners: [
+          {
+            event: 'fields.tabs.content.edit',
+            actions: [
+              {
+                component: 'Set',
+                name: 'fields.form.form.editable',
+                value: true
+              }
+            ]
+          },
+
+          {
+            event: 'fields.tabs.content.preview',
+            actions: [
+              {
+                component: 'Set',
+                name: 'fields.form.form.editable',
+                value: false
+              }
+            ]
+          }
+        ]
       }
-    }
-  ],
-
-  listeners: [
-    {
-      event: 'fields.tabs.content.edit',
-      actions: [
-        {
-          component: 'Set',
-          name: 'fields.form.form.editable',
-          value: true
-        }
-      ]
-    },
-
-    {
-      event: 'fields.tabs.content.preview',
-      actions: [
-        {
-          component: 'Set',
-          name: 'fields.form.form.editable',
-          value: false
-        }
-      ]
-    }
-  ]
-};
+    });
+  }
+}
